c0235bc29a877bae1f082d625a6de599184b9c06
MJH <mjh@MJHs-MacBook-Pro.local>
2019-03-17 02:05:36 +0900

    Initial Commit

---
+++ DuxCycler/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  DuxCycler
+//
+//  Created by MJH on 3/17/19.
+//  Copyright © 2019 MJH. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ DuxCycler/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  DuxCycler
+//
+//  Created by MJH on 3/17/19.
+//  Copyright © 2019 MJH. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
